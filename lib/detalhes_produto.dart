d9fe03601fb029a99b54c8c1a27be2e8578886de
LetyciaEst08 <letyciaesteves@hotmail.com>
2023-09-06 18:46:18 -0300

    criados containers para separar imagem de produto, descrição, ajuste na app bar

--- lib/detalhes_produto.dart
+++ lib/detalhes_produto.dart
@@ -45,101 +45,172 @@ class _DetalhesProdutoMaiorState extends State<DetalhesProdutoMaior> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(104, 231, 231, 231),
       appBar: AppBar(
-        title: Text(widget.produto.nome),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
       ),
-      body: ListView(
-        children: [
-          // Exiba a imagem em tamanho menor
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset(
-              widget.produto.imagem,
-              width: 150, // Defina o tamanho desejado
-              height: 150,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.0),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  widget.produto.imagem,
+                  width: MediaQuery.of(context).size.width -32, // Defina o tamanho desejado
+                  height: 150,
+                ),
+              ),
             ),
-          ),
-          Text('Nome: ${widget.produto.nome}'),
-          Text('Preço: R\$ ${widget.produto.preco.toStringAsFixed(2)}'),
-          Text('Descrição: ${widget.produto.descricao}'),
-
-          // Ícone de estrela para avaliação
-          RatingBar.builder(
-            initialRating: 0,
-            minRating: 1,
-            direction: Axis.horizontal,
-            allowHalfRating: true,
-            itemCount: 5,
-            itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-            itemBuilder: (context, _) => Icon(
-              Icons.star,
-              color: Colors.amber,
-            ),
-            onRatingUpdate: (rating) {
-              setState(() {
-                _rating = rating;
-              });
-            },
-          ),
-
-          // Espaço para comentário
-          _comentarioEnviado
-              ? SizedBox.shrink() // Oculta o espaço do comentário após o envio
-              : Column(
+             const SizedBox(height: 35.0),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.0),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextField(
-                      controller: _commentController,
-                      decoration: InputDecoration(
-                        labelText: 'Comentário',
+                    Text('Nome: ${widget.produto.nome}'),
+                    Text('Preço: R\$ ${widget.produto.preco.toStringAsFixed(2)}'),
+                    Text('Descrição: ${widget.produto.descricao}'),
+                  SizedBox(height: 7.0),
+                    // Ícone de estrela para avaliação
+                    RatingBar.builder(
+                      initialRating: 0,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemSize: 15.0,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
                       ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        _enviarComentario(); // Chama o método para enviar o comentário
-                      },
-                      child: Text('Enviar Comentário'),
-                    ),
-                  ],
-                ),
+                      onRatingUpdate: (rating) {
+                        setState(() {
+                          _rating = rating;
+                        });
+          },
+        ),
+      ],
+    ),
+  ),
+),
+            // Container(
+            //   width: MediaQuery.of(context).size.width,
+            //   decoration: BoxDecoration(
+            //     color: Colors.white,
+            //     boxShadow: [
+            //       BoxShadow(
+            //         color: Colors.grey.withOpacity(0.0),
+            //         spreadRadius: 3,
+            //         blurRadius: 5,
+            //         offset: Offset(0, 2),
+            //       ),
+            //     ],
+            //   ),
+             
+            // ),
 
-          // Espaço para sugestões de produtos
-          SizedBox(height: 20.0),
-          Text(
-            'Sugestões de Produtos',
-            style: TextStyle(fontSize: 18),
-          ),
-          // Lista de produtos sugeridos
-          Column(
-            children: produtosSugeridos.map((produto) {
-              return ListTile(
-                leading: Image.asset(
-                  produto.imagem,
-                  width: 50,
-                  height: 50,
-                ),
-                title: Text(produto.nome),
-                onTap: () {
-                  // Navegue para a página de detalhes deste produto
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => DetalhesProdutoMaior(
-                        produto: produto,
-                        todosProdutos: widget.todosProdutos,
+
+
+
+             // Espaço para comentário
+                    _comentarioEnviado
+                        ? SizedBox
+                            .shrink() // Oculta o espaço do comentário após o envio
+                        : Column(
+                            children: [
+                              TextField(
+                                controller: _commentController,
+                                decoration: InputDecoration(
+                                  labelText: 'Comentário',
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  _enviarComentario(); // Chama o método para enviar o comentário
+                                },
+                                child: Text('Enviar Comentário'),
+                              ),
+                            ],
+                          ),
+
+
+            // Espaço para sugestões de produtos
+            SizedBox(height: 20.0),
+            Text(
+              'Sugestões de Produtos',
+              style: TextStyle(fontSize: 18),
+            ),
+            // Lista de produtos sugeridos
+            Column(
+              children: produtosSugeridos.map((produto) {
+                return ListTile(
+                  leading: Image.asset(
+                    produto.imagem,
+                    width: 50,
+                    height: 50,
+                  ),
+                  title: Text(produto.nome),
+                  onTap: () {
+                    // Navegue para a página de detalhes deste produto
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => DetalhesProdutoMaior(
+                          produto: produto,
+                          todosProdutos: widget.todosProdutos,
+                        ),
                       ),
-                    ),
-                  );
-                },
-              );
-            }).toList(),
-          ),
-        ],
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
+    
     );
-  }
 
-  @override
-  void dispose() {
-    _commentController.dispose();
-    super.dispose();
+    @override
+    void dispose() {
+      _commentController.dispose();
+      super.dispose();
+    }
   }
 }
