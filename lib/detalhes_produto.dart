87483c6a4775b9112f0187a56ba22feb7d7ba7e0
LetyciaEst08 <letyciaesteves@hotmail.com>
2023-09-13 10:54:37 -0300

    Atualização final nos cards e scroll automatico nas sugestoes de produtos

--- lib/detalhes_produto.dart
+++ lib/detalhes_produto.dart
@@ -19,10 +19,16 @@ class _DetalhesProdutoMaiorState extends State<DetalhesProdutoMaior> {
   TextEditingController _commentController = TextEditingController();
   bool _comentarioEnviado = false;
   List<Produtos> produtosSugeridos = [];
+  ScrollController _scrollController = ScrollController();
+  double _scrollPosition = 0.0;
+  double _scrollMax = 0.0;
+  double _scrollIncrement = 1.0;
 
   @override
   void initState() {
     super.initState();
+    _scrollMax = 1000.0; // Defina o valor máximo de rolagem conforme necessário
+    _scrollAutomatically();
 
     // Encontre produtos relacionados com base no ID do produto atual
     produtosSugeridos = MeusProdutos.todosProdutos
@@ -30,6 +36,21 @@ class _DetalhesProdutoMaiorState extends State<DetalhesProdutoMaior> {
             produto.categoria == widget.produto.categoria &&
             produto != widget.produto)
         .toList();
+  }
+
+  void _scrollAutomatically() {
+    Future.delayed(Duration(seconds: 1), () {
+      if (_scrollPosition >= _scrollMax) {
+        _scrollPosition = 0.0;
+      }
+      _scrollController.animateTo(
+        _scrollPosition,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.ease,
+      );
+      _scrollPosition += _scrollIncrement;
+      _scrollAutomatically();
+    });
   }
 
   void _enviarAvaliacao() {
@@ -121,114 +142,127 @@ class _DetalhesProdutoMaiorState extends State<DetalhesProdutoMaior> {
               ),
             ),
             const SizedBox(height: 35.0),
-            Text(
-              'Sugestões de Produtos',
-              style: TextStyle(fontSize: 18),
-            ),
-            Container(
-              height: 100,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: produtosSugeridos.length,
-                itemBuilder: (context, index) {
-                  final produtoSugerido = produtosSugeridos[index];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DetalhesProdutoMaior(
-                            produto: produtoSugerido,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Produtos Sugeridos",
+                  style: TextStyle(
+                    fontSize: 20, // Tamanho da fonte do cabeçalho
+                    fontWeight: FontWeight.bold, // Estilo da fonte do cabeçalho
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  height: 100,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    controller: _scrollController,
+                    itemCount: produtosSugeridos.length,
+                    itemBuilder: (context, index) {
+                      final produtoSugerido = produtosSugeridos[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetalhesProdutoMaior(
+                                produto: produtoSugerido,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                produtoSugerido.imagem,
+                                width: 50,
+                                height: 50,
+                              ),
+                              Text(produtoSugerido.nome),
+                            ],
                           ),
                         ),
                       );
                     },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            produtoSugerido.imagem,
-                            width: 50,
-                            height: 50,
-                          ),
-                          Text(produtoSugerido.nome),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 35.0),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(242, 242, 241, 241),
-              ),
-              child: _comentarioEnviado
-                  ? const SizedBox.shrink()
-                  : Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.only(bottom: 2.0),
-                            child: Text(
-                              'Avalie este produto',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'roboto',
+                  ),
+                ),
+                const SizedBox(height: 35.0),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: _comentarioEnviado
+                      ? const SizedBox.shrink()
+                      : Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(bottom: 2.0),
+                                child: Text(
+                                  'Avalie este produto',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'roboto',
+                                  ),
+                                ),
                               ),
-                            ),
+                              const SizedBox(height: 1.0),
+                              const Text(
+                                'Compartilhe seus pensamentos com outros clientes',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                              const SizedBox(height: 1.0),
+                              RatingBar.builder(
+                                initialRating: 0,
+                                minRating: 1,
+                                direction: Axis.horizontal,
+                                allowHalfRating: true,
+                                itemCount: 5,
+                                itemSize: 25.0,
+                                itemPadding:
+                                    const EdgeInsets.symmetric(horizontal: 1.0),
+                                itemBuilder: (context, _) => const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                onRatingUpdate: (rating) {
+                                  setState(() {
+                                    _rating = rating;
+                                  });
+                                },
+                              ),
+                              TextField(
+                                controller: _commentController,
+                                decoration: const InputDecoration(
+                                  labelText: 'Comentário',
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  _enviarAvaliacao();
+                                  mostrarModalEnviado(
+                                      context); //Chama o método para enviar o comentário
+                                },
+                                child: const Text('Enviar Avaliação'),
+                              ),
+                            ],
                           ),
-                          const SizedBox(height: 1.0),
-                          const Text(
-                            'Compartilhe seus pensamentos com outros clientes',
-                            style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                          const SizedBox(height: 1.0),
-                          RatingBar.builder(
-                            initialRating: 0,
-                            minRating: 1,
-                            direction: Axis.horizontal,
-                            allowHalfRating: true,
-                            itemCount: 5,
-                            itemSize: 25.0,
-                            itemPadding:
-                                const EdgeInsets.symmetric(horizontal: 1.0),
-                            itemBuilder: (context, _) => const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                            ),
-                            onRatingUpdate: (rating) {
-                              setState(() {
-                                _rating = rating;
-                              });
-                            },
-                          ),
-                          TextField(
-                            controller: _commentController,
-                            decoration: const InputDecoration(
-                              labelText: 'Comentário',
-                              border: InputBorder.none,
-                            ),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              _enviarAvaliacao();
-                              mostrarModalEnviado(
-                                  context); //Chama o método para enviar o comentário
-                            },
-                            child: const Text('Enviar Avaliação'),
-                          ),
-                        ],
-                      ),
-                    ),
+                        ),
+                ),
+              ],
             ),
           ],
         ),
@@ -258,6 +292,7 @@ class _DetalhesProdutoMaiorState extends State<DetalhesProdutoMaior> {
   @override
   void dispose() {
     _commentController.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 }
